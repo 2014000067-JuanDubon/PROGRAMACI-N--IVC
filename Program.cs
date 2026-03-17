0f5547d21691c92aaf0d13069b4409a6903be34c
2014000067-JuanDubon <j.dubon.5adb@gmail.com>
2026-03-17 10:52:48 -0600

    Add files via upload
    Trabaje en clases

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numeros Positivos y Negativos");
+        //Variable
+        int num;
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("Soy un numero Positivo");
+        else
+            Console.WriteLine("Soy un numero Negativo");
+    }
+}
