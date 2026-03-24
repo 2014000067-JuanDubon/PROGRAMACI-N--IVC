6eddf50da442b9f5be70d064d5a92a9a0913a448
2014000067-JuanDubon <j.dubon.5adb@gmail.com>
2026-03-24 09:51:32 -0600

    Add files via upload
    Trabaje en clase 24/03/2026

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿internal class Program
 {
+    //un algortmo que determine si un alumno  aprueba o reprueba una materia basado en el promedio de 5calificaciones
     private static void Main(string[] args)
     {
-        Console.WriteLine("Numeros Positivos y Negativos");
-        //Variable
-        int num;
-        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("==Promedio de calificaciones==");
+        //Variables
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
 
-        if (num >= 0)
-            Console.WriteLine("Soy un numero Positivo");
+        //enviar info al usuario
+        Console.Write("Digite una materia: ");
+        materia = Console.ReadLine();
+        Console.Write("Dame 5 notas: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        n2 = Convert.ToInt32(Console.ReadLine());
+        n3 = Convert.ToInt32(Console.ReadLine());
+        n4 = Convert.ToInt32(Console.ReadLine());
+        n5 = Convert.ToInt32(Console.ReadLine());
+
+        //Proceso
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+
+        //Evaluar si gano perdio
+        if (promedio >= 70)
+            Console.WriteLine("El promedio es " + promedio + "y el estudiate Aprobo");
         else
-            Console.WriteLine("Soy un numero Negativo");
+            Console.WriteLine("El promedio es " + promedio + "y el estudiate Reprobo");
     }
 }
